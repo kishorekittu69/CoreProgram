1dbfabc9e337d831d76c8053c8c4b98cac284d16
kishohrekittu69@gmail.com <kishohrekittu69@gmail.com>
2023-03-02 19:07:42 +0530

    This is initial commit3

--- src/DemoJava.java
+++ src/DemoJava.java
@@ -1,6 +1,6 @@
 public class DemoJava {
 
     public static void main(String[] args) {
-        System.out.println("This is first program2");
+        System.out.println("This is first program3");
     }
 }
